2ddb82b1d8b724507b6cbf637508abd913c13651
ayir <riyasharma031995@gmail.com>
2017-04-27 03:22:04 +0530

    initial commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,4 +18,10 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', logout_page),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
+    url(r'^register/$', register),
+    url(r'^register/success/$', register_success),
+    url(r'^home/$', home),
 ]
